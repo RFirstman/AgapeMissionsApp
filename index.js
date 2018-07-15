a5e91a542c87d60b2ebdde80ec811978defe76f9
Robert Firstman <robfirstman@gmail.com>
2018-07-15 17:41:08 -0400

    bug fix

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ app.get("/health", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/build"));
+    app.use(express.static("client/build"));
 
     const path = require("path");
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
 }
 
